8b8d1b0ca16b9392a959335ed8db3d46e698f290
damdam166 <dericoo166@gmail.com>
2024-10-28 00:16:45 +0100

    feat[ALL]: loading screen done.

---
+++ resources/js/loading.js
@@ -0,0 +1,35 @@
+/* ------------------------------------------------------------------------- */
+/*                          LOADING SCREEN                                   */
+/* ------------------------------------------------------------------------- */
+
+window.addEventListener('load', () => {
+    const loadingScreen = document.getElementById('loading-screen');
+    if (!sessionStorage.getItem('hasVisited')) {
+        setTimeout(() => {
+            loadingScreen.style.opacity = '0';
+            setTimeout(() => {
+                loadingScreen.style.display = 'none';
+                sessionStorage.setItem('hasVisited', 'true');
+            }, 500); 
+        }, 2000); // Max time duration.
+    } else {
+        loadingScreen.style.display = 'none';
+    }
+});
+
+/* ------------------------------------------------------------------------- */
+/*                          PAGE TO PAGE LOADING                             */
+/* ------------------------------------------------------------------------- */
+
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', (event) => {
+        if (link.getAttribute('href') !== '#') {
+            event.preventDefault();
+            document.body.classList.add('fade-out');
+            setTimeout(() => {
+                window.location = link.getAttribute('href');
+            }, 300);
+        }
+    });
+});
+
