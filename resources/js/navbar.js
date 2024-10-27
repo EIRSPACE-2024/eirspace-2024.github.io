410099a676fe73305e2b3bc16520fbb1f4da2f38
damdam166 <dericoo166@gmail.com>
2024-10-27 20:02:08 +0100

    fix[BODY]: navbar drop down menu closes automatically when scrolling.

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -4,15 +4,23 @@
 
 let lastScrollTop = 0;
 const navbar = document.getElementById('navbar');
+const dropdownLinks = document.querySelector('.dropdown-links');
 
 /* ------------------------------------------------------------------------- */
 
 window.addEventListener('scroll', () => {
+    // Close the possible drop down menu.
+    if (dropdownLinks.classList.contains('active')) {
+        closeMenu();
+    }
+
     let st = window.pageYOffset || document.documentElement.scrollTop;
 
     if (st > lastScrollTop) {
         // Bottom.
         navbar.style.transform = 'translateY(-140%)';
+
+
     } else {
         // Upper.
         navbar.style.transform = 'translateY(0)';
@@ -25,18 +33,23 @@ window.addEventListener('scroll', () => {
 /* ------------------------------------------------------------------------- */
 
 function toggleMenu() {
-    const dropdownLinks = document.querySelector('.dropdown-links');
-    
     if (dropdownLinks.classList.contains('active')) {
-        // Closing it smoothly.
-        dropdownLinks.classList.add('closing');
-        
-        setTimeout(() => {
-            dropdownLinks.classList.remove('active', 'closing');
-        }, 500);
+        closeMenu();
     } else {
-        dropdownLinks.classList.add('active');
+        openMenu();
     }
+}
+
+function openMenu() {
+    dropdownLinks.classList.add('active');
+}
+
+function closeMenu() {
+    dropdownLinks.classList.add('closing');
+    
+    setTimeout(() => {
+        dropdownLinks.classList.remove('active', 'closing');
+    }, 500);
 }
 
 /* ------------------------------------------------------------------------- */
